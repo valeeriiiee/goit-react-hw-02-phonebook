f482cf6dec645b7c4b36d658acd4dd3ec1a8ca36
valeeriiiee <obleavalerie@gmail.com>
2024-06-24 23:30:22 +0800

    Update ContactListItem.jsx

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,3 +1,21 @@
-export const ContactListItem = () => {
-  return <div>ContactListItem</div>;
+import PropTypes from 'prop-types';
+
+export const ContactListItem = ({ filteredContact, deleteContact }) => {
+  // delete contact method
+  const handleDelete = () => {
+    deleteContact(filteredContact.id);
+  };
+
+  return (
+    <li>
+      <p>{filteredContact.name}</p>
+      <p>{filteredContact.number}</p>
+      <button onClick={handleDelete}>Delete</button>
+    </li>
+  );
+};
+
+ContactListItem.propTypes = {
+  filteredContact: PropTypes.object.isRequired,
+  deleteContact: PropTypes.func.isRequired,
 };
