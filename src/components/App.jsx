42ceff648c160da38a589a241c2e9b35a782f3b1
valeeriiiee <obleavalerie@gmail.com>
2024-06-24 23:52:19 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
 
 export class App extends Component {
   state = {
@@ -48,6 +49,7 @@ export class App extends Component {
         <h1>Phonebook</h1>
         <ContactForm addContact={this.addContact} contacts={contacts} />
         <h2>Contacts</h2>
+        <Filter filter={this.filter} setFilter={this.setFilter} />
         <ContactList
           filterContact={this.filterContact}
           deleteContact={this.deleteContact}
